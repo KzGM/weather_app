02f3bef2f90a1ef4a08206707e6fa61402f7aa05
Kozwiz <honghia12344@gmail.com>
2024-03-09 18:55:48 +0700

    GenderSelected

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:device_preview/device_preview.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
@@ -20,7 +22,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Weather App',
       theme: ThemeData(
-        textTheme: TextTheme(bodyMedium: TextStyle(color: Colors.white)),
+        textTheme: TextTheme(bodyMedium: TextStyle(color: Colors.black)),
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
@@ -47,192 +49,242 @@ class MyHomePage extends StatefulWidget {
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+enum Gender { male, female, other }
 
-  void _incrementCounter() {
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  Gender selectedGender = Gender.male;
+
+  void _Genderhandle(Gender? newGender) {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      selectedGender = newGender!;
     });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Colors.blue[200],
-        appBar: AppBar(
-          centerTitle: true,
-          backgroundColor: Colors.blue[200],
-          title: const Text(
-            '10.82, 206.24',
-            textAlign: TextAlign.center,
-            style: TextStyle(
-                color: Colors.white,
-                fontSize: 16.0,
-                fontWeight: FontWeight.w400),
-          ),
-          actions: [
-            IconButton(
-                onPressed: null,
-                icon: Icon(Icons.menu, color: Colors.white, size: 24.0))
+      body: Center(
+          child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+        Row(
+          children: [
+            _buildRadio('Male', Gender.male, Colors.blue),
+            SizedBox(
+              width: 10,
+            ),
+            _buildRadio('Female', Gender.female, Colors.red),
+            SizedBox(
+              width: 10,
+            ),
+            _buildRadio('Other', Gender.other, Colors.purple),
           ],
         ),
-        body: SingleChildScrollView(
-          child: Center(
-            child: Padding(
-              padding: const EdgeInsets.all(10.0),
-              child: Column(
-                children: [
-                  Container(
-                    child: Text(
-                      'Ho Chi Minh City',
-                      textAlign: TextAlign.center,
-                      style: TextStyle(
-                          fontSize: 24.0, fontWeight: FontWeight.w700),
-                    ),
-                  ),
-                  Container(
-                    child: Image.asset('assets/image/thunder_sun.png'),
-                  ),
-                  Container(
-                    child: Text(
-                      'Soft light',
-                      style: TextStyle(
-                          fontSize: 20.0, fontWeight: FontWeight.w400),
-                    ),
-                  ),
-                  Container(
-                    child: Text(
-                      '32°C',
-                      textAlign: TextAlign.center,
-                      style: TextStyle(
-                          fontSize: 64.0, fontWeight: FontWeight.w700),
-                    ),
-                  ),
-                  Container(
-                      child: Text(
-                    'Duststorm, sandstorm, drifting or blowing snow',
-                    textAlign: TextAlign.center,
-                    style:
-                        TextStyle(fontSize: 16.0, fontWeight: FontWeight.w400),
-                  )),
-                  SizedBox(
-                    height: 40,
-                  ),
-                  WeatherDetail()
-                ],
-              ),
-            ),
-          ),
-        ));
+        Text(
+          'Gender đang được chọn là $selectedGender',
+          textAlign: TextAlign.center,
+        )
+      ])),
+    );
   }
 
-  Widget WeatherDetail() {
-    return Opacity(
-      opacity: 0.4,
-      child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 16),
+  Widget _buildRadio(String title, Gender value, Color ActiveColor) {
+    return Expanded(
         child: Container(
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(8),
-            color: Colors.black,
-          ),
-          child: Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Column(
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    WeatherDetailItem(text: 'Humidity', value: '40%'),
-                    WeatherDetailItem(text: 'Wind', value: '2km/h')
-                  ],
-                ),
-                SizedBox(
-                  height: 10,
-                ),
-                Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      WeatherDetailItem(text: 'Humidity', value: '40%'),
-                      WeatherDetailItem(text: 'Humidity', value: '40%')
-                    ]),
-                SizedBox(
-                  height: 10,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    WeatherDetailItem(text: 'Humidity', value: '40%'),
-                    WeatherDetailItem(text: 'Humidity', value: '40%')
-                  ],
-                )
-              ],
+            decoration: BoxDecoration(
+              border: Border.all(
+                  color: selectedGender == value ? ActiveColor : Colors.grey),
+              borderRadius: BorderRadius.circular(8.0),
             ),
-          ),
-        ),
-      ),
-    );
-  }
-
-  Widget WeatherDetailItem({required String text, required String value}) {
-    return Text(
-      '$text $value',
-      style: TextStyle(fontSize: 20, fontWeight: FontWeight.w400),
-    );
-  }
-
-  Text Wind({
-    required String text,
-  }) {
-    return Text(
-      'Wind ${text}km/h',
-      style: TextStyle(fontSize: 20, fontWeight: FontWeight.w400),
-    );
-  }
-
-  Text PM10({
-    required String text,
-  }) {
-    return Text(
-      'PM10 $textμg/m³',
-      style: TextStyle(
-          color: Colors.white, fontSize: 20, fontWeight: FontWeight.w400),
-    );
-  }
-
-  Text Sunrise({
-    required String text,
-  }) {
-    return Text(
-      'Sunrise $text',
-      style: TextStyle(
-          color: Colors.white, fontSize: 20, fontWeight: FontWeight.w400),
-    );
-  }
-
-  Text UV({
-    required String text,
-  }) {
-    return Text(
-      'UV $text',
-      style: TextStyle(
-          color: Colors.white, fontSize: 20, fontWeight: FontWeight.w400),
-    );
-  }
-
-  Text Sunset({
-    required String text,
-  }) {
-    return Text(
-      'Sunset $text',
-      style: TextStyle(
-          color: Colors.white, fontSize: 20, fontWeight: FontWeight.w400),
-    );
+            child: RadioListTile<Gender>(
+              title: Text(
+                title,
+                style: TextStyle(fontSize: 10),
+              ),
+              value: value,
+              groupValue: selectedGender,
+              onChanged: _Genderhandle,
+              activeColor: ActiveColor,
+            )));
   }
 }
+
+// @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//         backgroundColor: Colors.blue[200],
+//         appBar: AppBar(
+//           centerTitle: true,
+//           backgroundColor: Colors.blue[200],
+//           title: const Text(
+//             '10.82, 206.24',
+//             textAlign: TextAlign.center,
+//             style: TextStyle(
+//                 color: Colors.white,
+//                 fontSize: 16.0,
+//                 fontWeight: FontWeight.w400),
+//           ),
+//           actions: [
+//             IconButton(
+//                 onPressed: null,
+//                 icon: Icon(Icons.menu, color: Colors.white, size: 24.0))
+//           ],
+//         ),
+//         body: SingleChildScrollView(
+//           scrollDirection: Axis.vertical,
+//           child: Center(
+//             child: Padding(
+//               padding: const EdgeInsets.all(10.0),
+//               child: Column(
+//                 children: [
+//                   Container(
+//                     child: Text(
+//                       'Ho Chi Minh City',
+//                       textAlign: TextAlign.center,
+//                       style: TextStyle(
+//                           fontSize: 24.0, fontWeight: FontWeight.w700),
+//                     ),
+//                   ),
+//                   Container(
+//                     child: Image.asset('assets/image/thunder_sun.png'),
+//                   ),
+//                   Container(
+//                     child: Text(
+//                       'Soft light',
+//                       style: TextStyle(
+//                           fontSize: 20.0, fontWeight: FontWeight.w400),
+//                     ),
+//                   ),
+//                   Container(
+//                     child: Text(
+//                       '32°C',
+//                       textAlign: TextAlign.center,
+//                       style: TextStyle(
+//                           fontSize: 64.0, fontWeight: FontWeight.w700),
+//                     ),
+//                   ),
+//                   Container(
+//                       child: Text(
+//                     'Duststorm, sandstorm, drifting or blowing snow',
+//                     textAlign: TextAlign.center,
+//                     style:
+//                         TextStyle(fontSize: 16.0, fontWeight: FontWeight.w400),
+//                   )),
+//                   SizedBox(
+//                     height: 40,
+//                   ),
+//                   WeatherDetail()
+//                 ],
+//               ),
+//             ),
+//           ),
+//         ));
+//   }
+
+//   Widget WeatherDetail() {
+//     return Opacity(
+//       opacity: 0.4,
+//       child: Padding(
+//         padding: const EdgeInsets.symmetric(horizontal: 16),
+//         child: Container(
+//           decoration: BoxDecoration(
+//             borderRadius: BorderRadius.circular(8),
+//             color: Colors.black,
+//           ),
+//           child: Padding(
+//             padding: const EdgeInsets.all(16.0),
+//             child: Column(
+//               children: [
+//                 Row(
+//                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
+//                   children: [
+//                     WeatherDetailItem(text: 'Humidity', value: '40%'),
+//                     WeatherDetailItem(text: 'Wind', value: '2km/h')
+//                   ],
+//                 ),
+//                 SizedBox(
+//                   height: 10,
+//                 ),
+//                 Row(
+//                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
+//                     children: [
+//                       WeatherDetailItem(text: 'Humidity', value: '40%'),
+//                       WeatherDetailItem(text: 'Humidity', value: '40%')
+//                     ]),
+//                 SizedBox(
+//                   height: 10,
+//                 ),
+//                 Row(
+//                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
+//                   children: [
+//                     WeatherDetailItem(text: 'Humidity', value: '40%'),
+//                     WeatherDetailItem(text: 'Humidity', value: '40%')
+//                   ],
+//                 )
+//               ],
+//             ),
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+
+//   Widget WeatherDetailItem({required String text, required String value}) {
+//     return Text(
+//       '$text $value',
+//       style: TextStyle(fontSize: 20, fontWeight: FontWeight.w400),
+//     );
+//   }
+
+//   Text Wind({
+//     required String text,
+//   }) {
+//     return Text(
+//       'Wind ${text}km/h',
+//       style: TextStyle(fontSize: 20, fontWeight: FontWeight.w400),
+//     );
+//   }
+
+//   Text PM10({
+//     required String text,
+//   }) {
+//     return Text(
+//       'PM10 $textμg/m³',
+//       style: TextStyle(
+//           color: Colors.white, fontSize: 20, fontWeight: FontWeight.w400),
+//     );
+//   }
+
+//   Text Sunrise({
+//     required String text,
+//   }) {
+//     return Text(
+//       'Sunrise $text',
+//       style: TextStyle(
+//           color: Colors.white, fontSize: 20, fontWeight: FontWeight.w400),
+//     );
+//   }
+
+//   Text UV({
+//     required String text,
+//   }) {
+//     return Text(
+//       'UV $text',
+//       style: TextStyle(
+//           color: Colors.white, fontSize: 20, fontWeight: FontWeight.w400),
+//     );
+//   }
+
+//   Text Sunset({
+//     required String text,
+//   }) {
+//     return Text(
+//       'Sunset $text',
+//       style: TextStyle(
+//           color: Colors.white, fontSize: 20, fontWeight: FontWeight.w400),
+//     );
+//   }
+// }
